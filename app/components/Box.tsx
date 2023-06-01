259ad473874262a521a55ae8a0ad3a3dc1977f59
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-06-01 17:32:15 +0200

    added event listeners and fought quite a bit with typescript over the event type.

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import { ThreeElements, ThreeEvent, useFrame, } from '@react-three/fiber';
+import React, { useRef } from 'react'
 
 interface BoxProps{
-  
    position:number[];
-   
+   name?:string;
 }
 
-const Box:React.FC<BoxProps> = ({position}) => {
+const Box:React.FC<BoxProps> = ({position,name}) => {
+
+   const meshRef = useRef<ThreeElements["mesh"]>(null);
+
+   useFrame((_,delta)=>{
+      meshRef.current && (meshRef.current.rotation.x += 1 * delta);
+      meshRef.current && (meshRef.current.rotation.y += 0.5 * delta);
+   })
+
    return (
-      <mesh position={position}>
+      <mesh
+         position={position} 
+         name={name}
+         ref={meshRef} 
+         onPointerOver={e => console.log((e as any).object.name)}
+      >
          <boxGeometry/>
          <meshBasicMaterial color={0x00ff00} wireframe/>
       </mesh>
    )
 }
 
-export default Box
+export default Box;
--- app/two-boxes/page.tsx
+++ app/two-boxes/page.tsx
@@ -2,6 +2,7 @@
 
 import { Canvas } from "@react-three/fiber"
 import Link from "next/link"
+
 import Box from "../components/Box"
 
 
@@ -10,14 +11,16 @@ import Box from "../components/Box"
 // and passing props to them
 
 const page = () => {
+
+   
    return (
       <div className="canvasContainer">
          <h1>This is the page with two boxes (that uses components)</h1>
          <Link href='/'>Go back home</Link>
 
          <Canvas camera={{position:[0,0,2]}}>
-            <Box position={[-0.75,0,0]}/>
-            <Box position={[0.75,0,0]}/>
+            <Box position={[-0.75,0,0]} name='A'/>
+            <Box position={[0.75,0,0]} name='B' />
          </Canvas>
       </div>
    )
